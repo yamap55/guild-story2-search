d0a70e9a7198380b31a78122b97772ac97a45bf6
yamap55 <tetuya120@hotmail.com>
2015-10-24 23:23:07 +0900

    initial commit.

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,34 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Hello world!
+ *
+ */
+@RestController
+@EnableAutoConfiguration
+public class App {
+
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!!!";
+	}
+
+	@RequestMapping("hoge")
+	String hoge() {
+		return "hoge";
+	}
+
+	@RequestMapping("huga")
+	String huga() {
+		return "huga";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
