16952d1cab863e24ccbeb047e1babf5d5106c241
yamap55 <tetuya120@hotmail.com>
2015-10-31 23:03:26 +0900

    APIを追加。

---
+++ src/main/java/com/yamap55/api/HogeRestController.java
@@ -0,0 +1,25 @@
+package com.yamap55.api;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.yamap55.domain.SuperRare;
+import com.yamap55.service.SuperRareService;
+
+@RestController
+@RequestMapping("api/super_rare")
+public class HogeRestController {
+
+	@Autowired
+	SuperRareService superRareService;
+
+	@RequestMapping(method = RequestMethod.GET)
+	List<SuperRare> getCustomers() {
+		List<SuperRare> superRares = superRareService.findAll();
+		return superRares;
+	}
+}
