5eeff75a158a17581af87701dde86f7d2d5b76b6
yamap55 <tetuya120@hotmail.com>
2015-11-11 23:15:03 +0900

    所持アイテムのサンプルテーブルを追加。

--- src/main/resources/db/migration/V1__create-schema.sql
+++ src/main/resources/db/migration/V1__create-schema.sql
@@ -1,3 +1,3 @@
 CREATE TABLE m_super_rare(id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(30), ability VARCHAR(500), pronunciation VARCHAR(30));
 CREATE TABLE m_item(id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(30), pronunciation VARCHAR(30));
-CREATE TABLE super_rare(id INT PRIMARY KEY AUTO_INCREMENT, super_rare_id INT, shogo INT, item_id VARCHAR(30), memo VARCHAR(30));
+CREATE TABLE items(id INT PRIMARY KEY AUTO_INCREMENT, super_rare_id INT, shogo INT, item_id VARCHAR(30), memo VARCHAR(30));
---
+++ src/main/resources/db/migration/V4__import-sample-super-rare.sql
@@ -0,0 +1,1 @@
+INSERT INTO items(super_rare_id, shogo, item_id, memo) VALUES(10,2,'hogehoge','memomemo');
