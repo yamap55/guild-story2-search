e79c5ae85aacaf826d97c6f7d9c9972824ee67c0
yamap55 <tetuya120@hotmail.com>
2015-11-28 00:17:21 +0900

    fixed #15 超レア名の存在チェックを追加。

--- src/main/java/com/yamap55/api/SuperRaraRestController.java
+++ src/main/java/com/yamap55/api/SuperRaraRestController.java
@@ -3,6 +3,7 @@ package com.yamap55.api;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +22,11 @@ public class SuperRaraRestController {
 	List<SuperRareMaster> getCustomers() {
 		List<SuperRareMaster> superRareMasters = superRareService.findAll();
 		return superRareMasters;
+	}
+
+	@RequestMapping(method = RequestMethod.POST, value = "existsByName")
+	boolean existsByName(@RequestBody String name) {
+		SuperRareMaster master = superRareService.findByName(name);
+		return master != null;
 	}
 }
