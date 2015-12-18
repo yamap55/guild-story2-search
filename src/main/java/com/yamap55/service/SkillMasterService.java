264f7189e8d2de70b7015d262391eefec5835d26
yamap55 <tetuya120@hotmail.com>
2015-12-18 23:47:20 +0900

    スキルマスターの検索を追加。

---
+++ src/main/java/com/yamap55/service/SkillMasterService.java
@@ -0,0 +1,20 @@
+package com.yamap55.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.yamap55.domain.SkillMaster;
+import com.yamap55.repository.SkillMasterRepository;
+
+@Service
+@Transactional
+public class SkillMasterService {
+
+	@Autowired
+	SkillMasterRepository skillMasterRepository;
+
+	public SkillMaster findByName(String name) {
+		return skillMasterRepository.findBySkillName(name);
+	}
+}
