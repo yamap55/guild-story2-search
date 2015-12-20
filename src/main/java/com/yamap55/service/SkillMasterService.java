e7a3936ce0b9f7164090e4cdb38b735ee429ee0c
yamap55 <tetuya120@hotmail.com>
2015-12-20 23:39:47 +0900

    スキルに関連付けられたアイテムを画面に表示。

--- src/main/java/com/yamap55/service/SkillMasterService.java
+++ src/main/java/com/yamap55/service/SkillMasterService.java
@@ -1,5 +1,7 @@
 package com.yamap55.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,5 +18,9 @@ public class SkillMasterService {
 
 	public SkillMaster findByName(String name) {
 		return skillMasterRepository.findBySkillName(name);
+	}
+
+	public List<SkillMaster> findAll() {
+		return skillMasterRepository.findAll();
 	}
 }
--- src/main/java/com/yamap55/web/PossessedItemsController.java
+++ src/main/java/com/yamap55/web/PossessedItemsController.java
@@ -9,9 +9,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.yamap55.domain.PossessedItem;
-import com.yamap55.domain.SuperRareMaster;
+import com.yamap55.domain.SkillMaster;
 import com.yamap55.service.ItemMasterService;
 import com.yamap55.service.PossessedItemsService;
+import com.yamap55.service.SkillMasterService;
 import com.yamap55.service.SuperRareMasterService;
 
 @Controller
@@ -27,13 +28,17 @@ public class PossessedItemsController {
 	@Autowired
 	ItemMasterService itemMasterService;
 
+	@Autowired
+	SkillMasterService skillMasterService;
+
 	@RequestMapping(method = RequestMethod.GET)
 	String list(Model model) {
 		List<PossessedItem> possessedItems = possessedItemsService.findAll();
 		model.addAttribute("possessedItems", possessedItems);
 
-		List<SuperRareMaster> superRares = superRareMasterService.findAll();
-		model.addAttribute("superRares", superRares);
+		List<SkillMaster> skills = skillMasterService.findAll();
+		model.addAttribute("skills", skills);
+
 		return "list";
 	}
 }
