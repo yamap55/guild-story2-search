fa51cf8bedb97975b8b2432aee51640f8b52e923
yamap55 <tetuya120@hotmail.com>
2015-12-26 02:17:39 +0900

    fixed #26 最新バージョンでは不要となったフィルターを除去。

--- src/main/java/com/yamap55/AppConfig.java
+++ src/main/java/com/yamap55/AppConfig.java
@@ -8,9 +8,6 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
-import org.springframework.web.filter.CharacterEncodingFilter;
 
 import net.sf.log4jdbc.Log4jdbcProxyDataSource;
 
@@ -33,14 +30,5 @@ public class AppConfig {
 	@Bean
 	DataSource dataSource() {
 		return new Log4jdbcProxyDataSource(this.dataSource);
-	}
-
-	@Order(Ordered.HIGHEST_PRECEDENCE)
-	@Bean
-	CharacterEncodingFilter characterEncodingFilter() {
-		CharacterEncodingFilter filter = new CharacterEncodingFilter();
-		filter.setEncoding("UTF-8");
-		filter.setForceEncoding(true);
-		return filter;
 	}
 }
