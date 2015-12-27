228ac42d78ac8e87fe6bc58ef6b386b6b32a3701
yamap55 <tetuya120@hotmail.com>
2015-12-28 00:21:33 +0900

    fixed #21 超レアマスターとスキルマスターを紐付け。

--- src/main/resources/db/migration/V1__create-schema.sql
+++ src/main/resources/db/migration/V1__create-schema.sql
@@ -1,5 +1,6 @@
-CREATE TABLE m_super_rare(id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(50), ability VARCHAR(500), pronunciation VARCHAR(50));
+CREATE TABLE m_super_rare(id INT PRIMARY KEY, name VARCHAR(50), pronunciation VARCHAR(50));
 CREATE TABLE m_item(id INT PRIMARY KEY, name VARCHAR(50), pronunciation VARCHAR(50));
 CREATE TABLE items(id INT PRIMARY KEY AUTO_INCREMENT, super_rare_master_id INT, shogo INT, item_master_id INT, memo VARCHAR(50));
 CREATE TABLE m_skill(id INT PRIMARY KEY, name VARCHAR(50), skill_type VARCHAR(50));
 CREATE TABLE item_skill(id INT PRIMARY KEY AUTO_INCREMENT, item_master_id INT, skill_master_id INT);
+CREATE TABLE super_rare_skill(id INT PRIMARY KEY AUTO_INCREMENT, super_rare_id INT, skill_master_id INT);
