0eee4bb6d5bc1b39716baa9d384d778c9846d95a
yamap55 <tetuya120@hotmail.com>
2016-02-14 00:16:14 +0900

    fixed #36 アイテムドロップ情報を修正。

--- src/main/resources/db/migration/V1__create-schema.sql
+++ src/main/resources/db/migration/V1__create-schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE m_super_rare(id INT PRIMARY KEY, name VARCHAR(50), pronunciation VARCHAR(50));
-CREATE TABLE m_item(id INT PRIMARY KEY, name VARCHAR(50), type_name VARCHAR(50), type_info VARCHAR(50), drop_location VARCHAR(50), price INT DEFAULT 0,attack INT DEFAULT 0,dexterity INT DEFAULT 0,defense INT DEFAULT 0,evasion INT DEFAULT 0,magic_attack INT DEFAULT 0,magic_defense INT DEFAULT 0,critical DOUBLE DEFAULT 0,attack_count DOUBLE DEFAULT 0,max_hp INT DEFAULT 0,trap_release INT DEFAULT 0,magic_recovery INT DEFAULT 0,pronunciation VARCHAR(50));
+CREATE TABLE m_item(id INT PRIMARY KEY, name VARCHAR(50), type_name VARCHAR(50), type_info VARCHAR(50), drop_location VARCHAR(255), price INT DEFAULT 0,attack INT DEFAULT 0,dexterity INT DEFAULT 0,defense INT DEFAULT 0,evasion INT DEFAULT 0,magic_attack INT DEFAULT 0,magic_defense INT DEFAULT 0,critical DOUBLE DEFAULT 0,attack_count DOUBLE DEFAULT 0,max_hp INT DEFAULT 0,trap_release INT DEFAULT 0,magic_recovery INT DEFAULT 0,pronunciation VARCHAR(50));
 CREATE TABLE items(id INT PRIMARY KEY AUTO_INCREMENT, super_rare_master_id INT, shogo INT, item_master_id INT, memo VARCHAR(50));
 CREATE TABLE m_skill(id INT PRIMARY KEY, name VARCHAR(50), skill_type VARCHAR(50));
 CREATE TABLE item_skill(id INT PRIMARY KEY AUTO_INCREMENT, item_master_id INT, skill_master_id INT);
